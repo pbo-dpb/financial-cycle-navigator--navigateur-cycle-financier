697ac553de76ca0cdd2c0dc1c02293e598770bd4
Remy Vanherweghem <remy.vanherweghem@parl.gc.ca>
2024-08-27 17:58:47 +0000

    Updated title.

--- vite.config.mjs
+++ vite.config.mjs
@@ -13,7 +13,7 @@ export default defineConfig({
   build: {
     manifest: true,
     rollupOptions: {
-      input: ['./src/main.js'],
+      input: ['./src/main.js', './index.html'],
     },
   }
 })
